0723b11aa508bde6236764f783f9baf04d8fd23a
Chad Easter <cpeaster@gmail.com>
2014-09-27 18:41:41 -0500

    Web Updates 3

--- js/main.js
+++ js/main.js
@@ -79,7 +79,6 @@ BRUSHED.slider = function(){
 		slides 					:  	[			// Slideshow Images
 											{image : 'img/slider-images/image01.jpg', title : '<div class="slide-content">Utility Tracker</div>', thumb : '', url : ''},
 											{image : 'img/slider-images/image02.jpg', title : '<div class="slide-content">Utility Tracker</div>', thumb : '', url : ''},
-											{image : 'img/slider-images/image03.jpg', title : '<div class="slide-content">Utility Tracker</div>', thumb : '', url : ''},
 											
 									],
 									
